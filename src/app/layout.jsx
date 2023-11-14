af775e059cefd52d1b334214d4dcfec87fa67e35
Mohammed fayis <90344973+mohmd-fayis@users.noreply.github.com>
2023-11-14 19:48:14 +0530

    favicon update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,25 +1,34 @@
-import '@/styles/globals.css'
-import { Roboto, Oxygen } from 'next/font/google'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/footer/Footer'
+import "@/styles/globals.css";
+import { Roboto, Oxygen } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/footer/Footer";
 
-const roboto = Roboto({ subsets: ['latin'], weight: ['100', '300', '700'], variable: '--font-roboto' })
-const oxygen = Oxygen({ subsets: ['latin-ext'], weight: ['300', '700'], variable: '--font-oxygen' })
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "300", "700"],
+  variable: "--font-roboto",
+});
+const oxygen = Oxygen({
+  subsets: ["latin-ext"],
+  weight: ["300", "700"],
+  variable: "--font-oxygen",
+});
 export const metadata = {
-  title: 'MCABEE',
-  description: 'Mcabee Technologies',
-}
+  title: "MCABEE",
+  description: "Mcabee Technologies",
+  icons: {
+    icon: "/icon.png",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${roboto.variable} ${oxygen.variable}`}>
         <Navbar />
-        <main className='app'>
-          {children}
-        </main>
+        <main className="app">{children}</main>
         <Footer />
       </body>
     </html>
-  )
+  );
 }
